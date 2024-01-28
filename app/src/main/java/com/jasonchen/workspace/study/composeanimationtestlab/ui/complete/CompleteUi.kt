e4849a4d6d76097e5d9a48fb441d73f6740af8ca
JasonChen <jason25325601@gmail.com>
2024-01-28 21:03:03 +0800

    [BeatCircle] 加上內外分層動畫

--- app/src/main/java/com/jasonchen/workspace/study/composeanimationtestlab/ui/complete/CompleteUi.kt
+++ app/src/main/java/com/jasonchen/workspace/study/composeanimationtestlab/ui/complete/CompleteUi.kt
@@ -1,13 +1,17 @@
 package com.jasonchen.workspace.study.composeanimationtestlab.ui.complete
 
+import android.util.Log
+import androidx.annotation.DrawableRes
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -16,6 +20,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,23 +30,42 @@ import com.jasonchen.workspace.study.composeanimationtestlab.R
 
 @Composable
 fun CompleteAnimation() {
-    AnimatedBeatCircle(
-        backgroundRes = R.drawable.img_cube_achieve, modifier = Modifier.size(100.dp)
+    CubeBeatCircle(
+        foregroundRes = R.drawable.img_cube_achieve_foreground,
+        backgroundRes = R.drawable.img_cube_achieve_background,
+        modifier = Modifier.size(100.dp)
     )
 }
 
 @Composable
+fun CubeBeatCircle(@DrawableRes foregroundRes: Int, @DrawableRes backgroundRes: Int, modifier: Modifier) {
+    var state by remember {
+        mutableStateOf(false)
+    }
+    AnimatedBeatCircle(backgroundRes = backgroundRes, modifier = modifier, onAnimationFinished = { state = true }) {
+        val animatedSize by animateFloatAsState(
+            targetValue = if (state) 1f else 0f, label = "", animationSpec = tween(durationMillis = 1000)
+        )
+
+        Icon(
+            painter = painterResource(id = foregroundRes), contentDescription = "", modifier = Modifier.alpha(animatedSize)
+        )
+    }
+}
+
+@Composable
 fun AnimatedBeatCircle(
-    backgroundRes: Int, modifier: Modifier, contentView: @Composable () -> Unit = {}
+    @DrawableRes backgroundRes: Int, modifier: Modifier, onAnimationFinished: ((Boolean) -> Unit)? = null, contentView: @Composable () -> Unit = {}
 ) {
     var state by remember {
         mutableStateOf(false)
     }
-    val animatedSize by animateFloatAsState(
-        targetValue = if (state) 0.75f else 0f, label = "", animationSpec = spring(
-            dampingRatio = 0.3f, stiffness = Spring.StiffnessLow
-        )
-    )
+    val animatedSize by animateFloatAsState(targetValue = if (state) 0.75f else 0f, label = "", animationSpec = spring(
+        dampingRatio = 0.3f, stiffness = Spring.StiffnessLow
+    ), finishedListener = {
+        onAnimationFinished?.invoke(true)
+        Log.e("TAG", "AnimatedBeatCircle: ")
+    })
 
     LaunchedEffect(key1 = animatedSize) {
         state = true
@@ -51,14 +76,12 @@ fun AnimatedBeatCircle(
             detectTapGestures(onTap = { state = !state })
         }, contentAlignment = Alignment.Center
     ) {
+        Surface(modifier = Modifier.fillMaxSize(animatedSize * 0.5f), color = Color.Transparent) {
+            contentView()
+        }
         Icon(
-            painter = painterResource(id = backgroundRes),
-            contentDescription = "",
-            modifier = Modifier.fillMaxSize(animatedSize)
+            painter = painterResource(id = backgroundRes), contentDescription = "", modifier = Modifier.fillMaxSize(animatedSize)
         )
-
-
-        contentView()
     }
 }
 
